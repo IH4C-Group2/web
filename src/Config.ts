1cfa5a71e7b613824d72620854bcf820e59b9e31
Fudzuki - Kusanagi <44330924+Fudzuki@users.noreply.github.com>
2024-05-14 13:35:42 +0900

    JsonWebToken を追加

--- src/Config.ts
+++ src/Config.ts
@@ -1,13 +1,16 @@
-// import { env } from 'process';
+import { env } from 'process';
 
-// const {
-//   CRYPTO_SECRET,
-// } = env;
+const {
+  // CRYPTO_SECRET,
+  JWT_SECRET
+} = env;
 
 // if (!CRYPTO_SECRET) throw new Error('CRYPTO_SECRET is not defined!');
+if (!JWT_SECRET) throw new Error('JWT_SECRET is not defined!');
 
 const Config = {
   // cryptoSecret: Buffer.from(CRYPTO_SECRET, 'base64'),
+  jwtSecret: JWT_SECRET,
   cookie: {
     session: '__ih14a_2',
     language: 'ja',
