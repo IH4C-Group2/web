d6c5e83f233f084a01c29e7bc0064783f938c924
Fudzuki <kiri11280201@gmail.com>
2024-05-15 15:52:41 +0900

    アカウント登録後のリダイレクト先をダッシュボードに変更

--- src/app/register/(components)/Form/index.tsx
+++ src/app/register/(components)/Form/index.tsx
@@ -13,7 +13,7 @@ const Form: FC<PropsWithChildren<Props>> = ({ children, action}) => {
   const handleSubmit = async (formdata: FormData) => {
     const res = await action(formdata);
 
-    if (res) redirect('/');
+    if (res) redirect('/dashboard');
 
     SetError(!res);
   }
