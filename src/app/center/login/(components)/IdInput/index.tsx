3a323ee54fb117ab928b52dc4b5a79834197fd1c
Takahashi Yuto <yu6493to7142@gmail.com>
2024-05-24 16:52:01 +0900

    センターのログインです

---
+++ src/app/center/login/(components)/IdInput/index.tsx
@@ -0,0 +1,11 @@
+import type { FC } from 'react';
+
+const IdInput: FC = () => {
+  return (
+    <div>
+      <input type='text' name='Id' />
+    </div>
+  );
+};
+
+export default IdInput;
---
+++ src/app/center/login/actions.ts
@@ -0,0 +1,33 @@
+'use server';
+
+import { cookies } from 'next/headers';
+import { SHA256 } from 'crypto-js';
+import { sign } from 'jsonwebtoken';
+
+import { prisma } from "@/utils/prisma";
+import Config from '@/Config';
+
+export const login = async (formData: FormData) => {
+  const password = formData.get('password')?.toString();
+  const loginId = formData.get('id')?.toString();
+
+  if (!password || !loginId) return false;
+
+  const transportationUser = await prisma.transportationUser.findUnique({
+    where: {
+      loginId,
+      password: SHA256(password).toString()
+    }
+  });
+
+  if (!transportationUser) return false;
+
+  const token = sign({
+    id: transportationUser.transportationUserId,
+    type: 'center',
+  }, Config.jwtSecret);
+
+  cookies().set(Config.cookie.session, token);
+
+  return true;
+}
