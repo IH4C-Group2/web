337211c0854a43778f35051766fbcadce4e5c236
Fudzuki <kiri11280201@gmail.com>
2024-06-07 17:36:46 +0900

    fix

--- src/app/Transportation/dashboard/page.tsx
+++ src/app/Transportation/dashboard/page.tsx
@@ -1,7 +1,6 @@
 import { FC } from 'react';
 import Link from 'next/link';
 import Logout from '@/app/layouts/Logout';
-import Factorykanri from '@/app/layouts/Factory/factory_kanri';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faIdCard, faClockRotateLeft, } from '@fortawesome/free-solid-svg-icons';
 
