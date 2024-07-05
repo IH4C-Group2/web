1570073b2cd046e25f0e6a63af00eefd6bf0c544
Fupamu <creeper.ayu0605mc@icloud.com>
2024-07-05 15:09:09 +0900

    ログイン画面全部

--- src/app/driver/login/page.tsx
+++ src/app/driver/login/page.tsx
@@ -14,25 +14,26 @@ const Login: FC = () => {
   };
 
   return (
+
     <div className="min-h-screen">
       <div className="w-full py-4 bg-blue-900">
-      <h1 className='text-white text-2xl flex justify-center items-center'>ログイン画面</h1>
+        <h1 className='text-white text-2xl flex justify-center items-center'>ドライバーログイン画面</h1>
       </div>
       <Form action={handleSubmit}>
 
-    <div className='mt-32'>
+        <div className='mt-16 lg:mt-32'>
 
-    <p className='ml-80 mb-2 font-semibold'>管理者ID</p>
-        <div className="mx-20 mb-32 flex justify-center">
-          <IdInput />
+          <p className='ml-8 lg:ml-80 mb-2 font-semibold'>ログインID</p>
+          <div className="mx-4 lg:mx-20 mb-8 lg:mb-32 flex justify-center">
+            <IdInput />
+          </div>
+
+          <p className='ml-8 lg:ml-80 mb-2 font-semibold'>パスワード</p>
+          <div className="mx-4 lg:mx-20 mb-8 lg:mb-32 flex justify-center">
+            <PasswordInput />
+          </div>
+
         </div>
-
-        <p className='ml-80 mb-2 font-semibold'>パスワード</p>
-        <div className="mx-20 mb-32 flex justify-center">
-          <PasswordInput />
-        </div>
-
-      </div>
 
         <div className='flex justify-center'>
           <LoginButton />
--- src/app/hitachi/login/page.tsx
+++ src/app/hitachi/login/page.tsx
@@ -4,6 +4,7 @@ import { login } from './actions';
 import Form from './(components)/Form';
 import IdInput from '@/app/layouts/LoginForm/IdInput';
 import PasswordInput from '@/app/layouts/LoginForm/PasswordInput';
+import LoginButton from '@/app/layouts/LoginForm/LoginButton';
 
 const Login: FC = () => {
   const handleSubmit = async (formdata: FormData) => {
@@ -13,12 +14,30 @@ const Login: FC = () => {
   };
 
   return (
-    <div className="min-h-screen">
-      <h1>ログイン画面</h1>
+<div className="min-h-screen">
+      <div className="w-full py-4 bg-blue-900">
+        <h1 className='text-white text-2xl flex justify-center items-center'>HITACHIログイン画面</h1>
+      </div>
       <Form action={handleSubmit}>
-        <IdInput />
-        <PasswordInput />
-        <button type="submit">ログイン</button>
+
+        <div className='mt-16 lg:mt-32'>
+
+          <p className='ml-8 lg:ml-80 mb-2 font-semibold'>ログインID</p>
+          <div className="mx-4 lg:mx-20 mb-8 lg:mb-32 flex justify-center">
+            <IdInput />
+          </div>
+
+          <p className='ml-8 lg:ml-80 mb-2 font-semibold'>パスワード</p>
+          <div className="mx-4 lg:mx-20 mb-8 lg:mb-32 flex justify-center">
+            <PasswordInput />
+          </div>
+
+        </div>
+
+        <div className='flex justify-center'>
+          <LoginButton />
+        </div>
+
       </Form>
     </div>
   );
