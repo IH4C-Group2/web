4a562d775b823f31c3943d5ccd3fc859bea5775e
K1zuna <98415588+K1zuna-net@users.noreply.github.com>
2024-07-11 17:17:38 +0900

    feat:transportationUser-edit (#47)
    * fin
    
    * スケジュールを戻す
    
    ---------
    
    Co-authored-by: fudzuki <kiri11280201@gmail.com>

---
+++ src/app/factory/edit/[scheduleListId]/(components)/ListIdInput/index.tsx
@@ -0,0 +1,12 @@
+import type { FC } from "react";
+
+const ListId: FC = () => {
+    return (
+        <div>
+            <label>スケジュールID</label>
+            <input type="text" name="ListId" />
+        </div>
+    );
+};
+
+export default ListId;
